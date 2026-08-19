9a7ebea73c2050eb5059a806c6bf5198219441e0
Sushant Kumar <sushant843120@gmail.com>
2026-08-19 16:34:29 +0530

    style(hrms): format leave service application

--- backend/leave-service/src/main/java/com/ums/hrms/leave/LeaveServiceApplication.java
+++ backend/leave-service/src/main/java/com/ums/hrms/leave/LeaveServiceApplication.java
@@ -7,5 +7,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class LeaveServiceApplication {
-    public static void main(String[] args) { SpringApplication.run(LeaveServiceApplication.class, args); }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveServiceApplication.class, args);
+    }
 }
