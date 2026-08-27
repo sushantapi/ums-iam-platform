8432bfc4d907e2cc592deb59fd106a6957f17b47
Sushant Kumar <sushant843120@gmail.com>
2026-08-27 18:57:52 +0530

    feat(notification): enable user directory feign client

--- backend/notification-service/src/main/java/com/ums/notification/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/ums/notification/NotificationServiceApplication.java
@@ -2,14 +2,15 @@ package com.ums.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients(basePackages = "com.ums.notification.client")
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
-
 }
